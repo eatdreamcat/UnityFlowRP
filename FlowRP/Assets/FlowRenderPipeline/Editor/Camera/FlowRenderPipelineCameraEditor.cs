9df0042d84a758ef8226093e3e91b98bcb86200f
jingwen.wang <jingwen.wang@dena.com>
2023-06-06 21:54:26 +0800

    Pipeline Setup

---
+++ FlowRP/Assets/FlowRenderPipeline/Editor/Camera/FlowRenderPipelineCameraEditor.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Rendering.FlowPipeline;
+
+namespace UnityEditor.Rendering.FlowPipeline
+{
+  
+    [CustomEditorForRenderPipeline(typeof(Camera), typeof(FlowRenderPipelineAsset))]
+    [CanEditMultipleObjects]
+    class FlowRenderPipelineCameraEditor : CameraEditor
+    {
+        
+        FlowRenderPipelineSerializedCamera m_SerializedCamera;
+        
+        public new void OnEnable()
+        {
+            base.OnEnable();
+            m_SerializedCamera = new FlowRenderPipelineSerializedCamera(serializedObject, settings);
+            
+        }
+
+        void UpdateCameras()
+        {
+            m_SerializedCamera.Refresh();
+        }
+        
+        public new void OnDisable()
+        {
+            base.OnDisable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var rpAsset = FlowRenderPipeline.asset;
+            if (rpAsset == null)
+            {
+               
+                return;
+            }
+
+            m_SerializedCamera.Update();
+        }
+    }
+
+}
